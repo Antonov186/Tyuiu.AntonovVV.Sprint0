67bd7951d2ea2a20775e921501efe9e1b51c4965
Василий Антонов <vasya.hvostic@gmail.com>
2024-12-04 17:07:46 +0500

    Task2

---
+++ Tyuiu.AntonovVV.Sprint0.Task2.V00.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AntonovVV.Sprint0.Task2.V00.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AntonovVV.Sprint0.Task2.V00.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1655847b-4edb-4532-87de-75ba3ba1c718")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

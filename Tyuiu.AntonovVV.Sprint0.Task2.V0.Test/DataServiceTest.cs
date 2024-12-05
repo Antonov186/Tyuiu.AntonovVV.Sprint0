ae1e9023cf56183dc5ada27ecd51a9bcedb82377
Василий <vasya.hvostic@gmail.com>
2024-12-05 21:44:58 +0500

    Add files via upload

---
+++ Tyuiu.AntonovVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.AntonovVV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.AntonovVV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Василий";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Василий", res);
+
+        }
+    }
+}
